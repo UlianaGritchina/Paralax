30f802ab84edad53b9d0e5f383a239a3f0d95281
Uliana Gritchina <ugritchina@icloud.com>
2022-12-23 17:55:45 +0300

    Initial Commit

---
+++ Paralax/ParalaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParalaxApp.swift
+//  Paralax
+//
+//  Created by Ульяна Гритчина on 23.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ParalaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
